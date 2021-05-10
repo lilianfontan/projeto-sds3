e1be4bf55bedd98e7745c5d2ef38749d1728bb69
Lilian Fontan <lilian.fontan@gmail.com>
2021-05-09 23:33:37 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,27 @@
+import { saller } from "./saller"
+
+export type Sale = {
+        id: number,
+        visited: number,
+        deals: number,
+        amount: number,
+        date: string,
+        seller: saller; 
+    
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean,
+    totalElements: number,
+    totalPages: number,
+    size?: number,
+    number: number,
+    first: boolean,
+    numberOfElements?: number,
+    empty?: boolean
+}
+
 export type SaleSum =  {
     sellerName: string;
     sum: number;
